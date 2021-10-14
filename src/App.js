b32c225c047e895c34758e0c9a2414a9acc9c9f4
abhishekmittal18 <abhishek.mittal1806@gmail.com>
2021-10-14 12:12:11 +0530

    my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class FetchApi extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            items: [],
+            isLoaded: false
+        };
+    }
+    
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(res => res.json())
+        .then(result => {
+            this.setState({
+                isLoaded: true,
+                items: result
+            });
+        })
+    }
 
-export default App;
+    render(){
+        const {items, isLoaded} = this.state;
+        if(!isLoaded) 
+        return <div><h1>Please wait...</h1></div>
+        return(
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id}>
+                        <h1>{item.title}</h1>
+                         <img src={item.url}></img> 
+                    </li>
+                ))
+                }
+            </ul>
+        );
+            }
+
+    }
+export default FetchApi;
